830b78d431f3b062b1caaf390fd031490136da3c
JJ <warmachinesocial@skiff.com>
2025-08-11 12:35:51 -0500

    Fix - Terminating connection should remove the peers on both sides

--- app/js/app.js
+++ app/js/app.js
@@ -9,51 +9,64 @@ console.log('Application starting...')
 
 // IPC Event Listeners
 ipc.on('start', async (data) => {
-  const documentsPath = data['path']
-  try {
-    await hyperbeeManager.initializeHyperbee(documentsPath)
-    const keyPair = await hyperbeeManager.getOrCreateKeyPair()
-    await hyperswarmManager.initializeHyperswarm(keyPair)
+    const documentsPath = data['path']
+    try {
+        await hyperbeeManager.initializeHyperbee(documentsPath)
+        const keyPair = await hyperbeeManager.getOrCreateKeyPair()
+        await hyperswarmManager.initializeHyperswarm(keyPair)
 
-    // Setup and register all protocols
-//    await mapManager.getMaps(documentsPath)
-    locationManager.setupLocationProtocol()
-    
-    console.log('All managers initialized and protocols registered.')
-  } catch (error) {
-    console.error('Failed to start application:', error)
-  }
+        // Setup and register all protocols
+        //    await mapManager.getMaps(documentsPath)
+        locationManager.setupLocationProtocol()
+
+        console.log('All managers initialized and protocols registered.')
+    } catch (error) {
+        console.error('Failed to start application:', error)
+    }
 })
 
 ipc.on('requestPublicKey', async () => {
-  try {
-    const publicKey = await hyperbeeManager.getPublicKeyFromDb()
-    if (publicKey) {
-      ipc.send('publicKeyResponse', { publicKey: publicKey })
-    } else {
-      console.warn('Public key not found in database.')
+    try {
+        const publicKey = await hyperbeeManager.getPublicKeyFromDb()
+        if (publicKey) {
+            ipc.send('publicKeyResponse', { publicKey: publicKey })
+        } else {
+            console.warn('Public key not found in database.')
+        }
+    } catch (error) {
+        console.error('Error requesting public key:', error)
     }
-  } catch (error) {
-    console.error('Error requesting public key:', error)
-  }
 })
 
 ipc.on('joinPeer', async (data) => {
-  const peerPublicKey = data
-  console.log('Received "joinPeer" event for:', peerPublicKey)
-  try {
-    hyperswarmManager.joinPeer(peerPublicKey)
-  } catch (error) {
-    console.error('Failed to join peer:', error)
-  }
-})
+    const peerPublicKey = data;
+    console.log('Received "joinPeer" event for:', peerPublicKey);
+    try {
+        //        await hyperbeeManager.addPeer(peerPublicKey);
+        console.log("Key before joining the swarm" + peerPublicKey)
+        hyperswarmManager.joinPeer(peerPublicKey);
+    } catch (error) {
+        console.error('Failed to join peer:', error);
+    }
+});
+
+ipc.on('leavePeer', async (data) => {
+    const peerPublicKey = data;
+    console.log('Received "leavePeer" event for:', peerPublicKey);
+    try {
+        //        await hyperbeeManager.removePeer(peerPublicKey);
+        hyperswarmManager.leavePeer(peerPublicKey);
+        hyperswarmManager.closeConnection(peerPublicKey);
+    } catch (error) {
+        console.error('Failed to leave peer:', error);
+    }
+});
 
 ipc.on('locationUpdate', async (data) => {
-  console.log('Received "locationUpdate" event.')
-  try {
-    // Route the IPC message to the new location manager
-    locationManager.sendLocationToPeers(data)
-  } catch (error) {
-    console.error('Failed to send user location:', error)
-  }
+    console.log('Received "locationUpdate" event.')
+    try {
+        locationManager.sendLocationToPeers(data)
+    } catch (error) {
+        console.error('Failed to send user location:', error)
+    }
 })
