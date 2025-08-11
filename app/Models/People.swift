525b61cb34fedf420d72d50d97c78b7ca23f54f7
JJ <warmachinesocial@skiff.com>
2025-08-11 12:34:52 -0500

    Fix - Refactored Model

--- app/Models/People.swift
+++ app/Models/People.swift
@@ -5,22 +5,12 @@
 //  Created by joker on 2025-01-15.
 //
 
-import SwiftData
 import Foundation
 
-@Model
-class People {
-    @Attribute(.unique) var id: String
+struct People: Codable, Identifiable {
+    var id: String
     var name: String?
     var image: Data?
     var latitude: Double?
     var longitude: Double?
-    
-    init(id: String, name: String? = nil, image: Data? = nil, latitude: Double? = nil, longitude: Double? = nil) {
-        self.id = id
-        self.name = name
-        self.image = image
-        self.latitude = latitude
-        self.longitude = longitude
-    }
 }
