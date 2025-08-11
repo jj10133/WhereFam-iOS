cb8895d9a53c4240f3762ba82a32c63a88fdbc11
JJ <warmachinesocial@skiff.com>
2025-08-11 12:34:18 -0500

    Added - ThanksView.swift

---
+++ app/Core/Home/View/ThanksView.swift
@@ -0,0 +1,18 @@
+//
+//  ThanksView.swift
+//  App
+//
+//  Created by joker on 2025-08-11.
+//
+
+import SwiftUI
+
+struct ThanksView: View {
+    var body: some View {
+        Text("Thank you for your support! ❤️")
+    }
+}
+
+#Preview {
+    ThanksView()
+}
